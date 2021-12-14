133bde181c8bfb43796730d8a6ada61e99dfc241
Sergio Mejia <sdmejiac@correo.udistrital.edu.co>
2021-12-13 22:28:42 -0500

    Estilos agragados en Préstamos

--- index.php
+++ index.php
@@ -4,10 +4,9 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="css/style.css">
     <title>Formulario Nomina</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
@@ -22,7 +21,7 @@
         </div>
     </nav>
     </div>
-
+<br><br><br>
     <a href="#"></a>
     
     <div class="container">
@@ -32,7 +31,7 @@
             <form action="" class="row g-3" method="post">
                 <div class="col-md-12">
                     <label class="form-label">Nombre</label>
-                    <input type="text" class="form-control" id="name" name="name">
+                    <input type="text" class="form-control" id="nombre" name="nombre">
                 </div>
                 <div class="col-md-6">
                     <label class="form-label">Cedula</label>
--- php/prestamos.php
+++ php/prestamos.php
@@ -4,102 +4,108 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="css/style.css">
-    <title>Formulario Nomina</title>
+    <title>Nomina</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 
 <body>
-    <nav class=" navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-        <a class="navbar-brand" href="../index.php">Registar</a>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item"><a class="nav-link" href="./mostrar.php">Datos</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Préstamos</a></li>
-            </ul>
-        </div>
-    </nav>
+    <div>
+        <nav class=" navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+            <a class="navbar-brand" href="../index.php">Registar</a>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item"><a class="nav-link" href="./mostrar.php">Datos</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Préstamos</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
     <br><br><br>
 
-    <form action="" method="post">
-        <table border="0" align="center">
-            <tr>
-                <td>Cédula</td>
-                <td><input type="number" name="cedula" id="cedula"></td>
-            </tr>
-            <tr>
-                <td>Monto</td>
-                <td><input type="number" name="monto" id="monto"></td>
-            </tr>
-            <tr>
-                <td>Cuotas</td>
-                <td><input type="number" name="cuotas" id="cuotas"></td>
-            </tr>
-            <tr>
-                <td>Cuotas pagadas</td>
-                <td><input type="number" name="pago" id="pago"></td>
-            </tr>
-            <tr>
-                <td>Fecha</td>
-                <td><input type="date" name="fecha" id="fecha"></td>
-            </tr>
-            <tr>
-                <td colspan="2" align="center">
-                    <input type="submit" name="enviar" value="Registrar" id="enviar" class="btn btn-primary">
-                </td>
-            </tr>
-        </table>
-    </form>
-    <?php
+    <div class="container">
+        <div class="cont">
+            <form action="" class="row g-3" method="post">
+                <h1>Préstamos</h1>
+                <hr class="bg bg-light">
+                <div class="col-md-12">
+                    <label class="form-label">Cedula</label>
+                    <td><input type="number" name="cedula" id="cedula" class="form-control"></td>
+                </div>
+                <div class="col-md-6">
+                    <label class="form-label">Monto</label>
+                    <td><input type="number" name="monto" id="monto" class="form-control"></td>
+                </div>
+                <div class="col-md-6">
+                    <label class="form-label">Cuotas</label>
+                    <td><input type="number" name="cuotas" id="cuotas" class="form-control"></td>
+                </div>
+                <div class="col-md-6">
+                    <label class="form-label">Cuotas Pagadas</label>
+                    <td><input type="number" name="pago" id="pago" class="form-control"></td>
+                </div>
+                <div class="col-md-6">
+                    <label class="form-label">Fecha</label>
+                    <td><input type="date" name="fecha" id="fecha" class="form-control"></td>
+                </div>
+                <div class="col-md-12" align="center">
+                    <input type="submit" class="btn btn-success btn-lg" name="enviar" id="enviar" value="Registrar" />
+                </div>
+            </form>
+        </div>
+    </div>
 
-    include('./conexion.php');
-    if (isset($_POST['enviar'])) {
-        $conex = conectar();
-
-
-        $cedula = $_POST['cedula'];
-        $monto = $_POST['monto'];
-        $cuotas = $_POST['cuotas'];
-        $fecha = $_POST['fecha'];
-        $cpago = $_POST['pago'];
-
-
-        $vcuota = $monto / $cuotas;
-        $cdescuento = $cuotas - $cpago;
-        $saldo = (($cuotas - $cpago) * ($monto / $cuotas));
-
-
-        if ($cuotas < $cpago) {
-            echo "<script>alert('No es posible ingresar el número de cuotas pagadas')</script>";
-        } else {
-            $sql = "update `empleado` set `Monto`='$monto',`Cuotas`='$cuotas',`fecha`='$fecha',
-                `cuotas_pagadas`='$cpago',`cuotas_descuento`='$cdescuento',`valor_cuota`='$vcuota',
-                `saldoP` ='$saldo' where `Cedula` = '$cedula'";
-
-            $resultado = mysqli_query($conex, $sql) or die("Error en la consulta $sql" . mysqli_error($conex));
-
-            $deducciones = "select `total_deduccions`, `total_nomina` from `empleado` where `Cedula`='$cedula'";
-            $result =  mysqli_query($conex, $deducciones) or die("Error en la consulta $sql" . mysqli_error($conex));
-
-            $row = mysqli_fetch_array($result);
-            $ded = $row['total_deduccions']; 
-            $nom = $row['total_nomina'];
-
-            $ded = $ded + $vcuota;
-            $nom = $nom - $vcuota;
-
-            $sql2 = "update `empleado` set `total_deduccions`= '$ded', `total_nomina`='$nom' where `Cedula` = '$cedula'";
-            mysqli_query($conex, $sql2) or die("Error en la consulta $sql" . mysqli_error($conex));
-
-            if ($resultado == 1) {
-                echo "<script>alert('El usuario se registró correctamente')</script>";
-            }
-            mysqli_close($conex);
-        }
-    }
-    ?>
-
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 </body>
 
 </html>
+
+<?php
+
+include('./conexion.php');
+if (isset($_POST['enviar'])) {
+    $conex = conectar();
+
+
+    $cedula = $_POST['cedula'];
+    $monto = $_POST['monto'];
+    $cuotas = $_POST['cuotas'];
+    $fecha = $_POST['fecha'];
+    $cpago = $_POST['pago'];
+
+
+    $vcuota = $monto / $cuotas;
+    $cdescuento = $cuotas - $cpago;
+    $saldo = (($cuotas - $cpago) * ($monto / $cuotas));
+
+
+    if ($cuotas < $cpago) {
+        echo "<script>alert('No es posible ingresar el número de cuotas pagadas')</script>";
+    } else {
+        $sql = "update `empleado` set `Monto`='$monto',`Cuotas`='$cuotas',`fecha`='$fecha',
+                `cuotas_pagadas`='$cpago',`cuotas_descuento`='$cdescuento',`valor_cuota`='$vcuota',
+                `saldoP` ='$saldo' where `Cedula` = '$cedula'";
+
+        $resultado = mysqli_query($conex, $sql) or die("Error en la consulta $sql" . mysqli_error($conex));
+
+        $deducciones = "select `total_deduccions`, `total_nomina` from `empleado` where `Cedula`='$cedula'";
+        $result =  mysqli_query($conex, $deducciones) or die("Error en la consulta $sql" . mysqli_error($conex));
+
+        $row = mysqli_fetch_array($result);
+        $ded = $row['total_deduccions'];
+        $nom = $row['total_nomina'];
+
+        $ded = $ded + $vcuota;
+        $nom = $nom - $vcuota;
+
+        $sql2 = "update `empleado` set `total_deduccions`= '$ded', `total_nomina`='$nom' where `Cedula` = '$cedula'";
+        mysqli_query($conex, $sql2) or die("Error en la consulta $sql" . mysqli_error($conex));
+
+        if ($resultado == 1) {
+            echo "<script>alert('El usuario se registró correctamente')</script>";
+        }
+        mysqli_close($conex);
+    }
+}
+?>
